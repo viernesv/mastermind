6fd354c94f89143367070695003c13a3955084b4
viernesv <77942889+viernesv@users.noreply.github.com>
2022-05-24 21:45:18 -1000

    Update board.py

--- mastermind/board.py
+++ mastermind/board.py
@@ -1,62 +1,57 @@
 import pygame
 from mastermind.constants import *
 
-difficulty = 'Normal'
+
 class Board():
     def __init__(self):
         self.pieces = []
-        self.secret = []
+        self.difficulty = DIFFICULTY
+        self.max_attempts = MAX_ATTEMPTS
 
         self.set_pieces()
 
     def draw_board(self, window):
-        # load board background
         WINDOW.blit(background, (0, 0))
 
-        # draw game option buttons
-        for i in range(len(button_collection)):
-            self.draw_item(window, button_collection[i], 50 + 150*i, 200)
+        for i in range(len(button_collection)):                                                     # draw game option buttons
+            self.draw_item(window, button_collection[i], 50 + 225*i, 200)
         
-        # draw pieces on Easy difficulty
-        for i in range(len(easy_piece_collection)):
+        for i in range(len(easy_piece_collection)):                                                 # draw pieces of Easy difficulty
             self.draw_item(window, easy_piece_collection[i], 200 + SQUARE_SIZE*i, 925)
         
-        # draw pieces on Normal/Hard difficulty
-        if difficulty != 'Easy':
+        difficulty = self.get_difficulty()                                  
+        if difficulty != 'Easy':                                                                    # draw pieces of Normal/Hard difficulty
             for i in range(len(normal_piece_collection)):
                 self.draw_item(window, normal_piece_collection[i], 150 + SQUARE_SIZE*5*i, 925)
             if difficulty == 'Hard':
                 for i in range(len(normal_piece_collection)):
                     self.draw_item(window, hard_piece_collection[i], 100 + SQUARE_SIZE*7*i, 925)
 
-        # draw secret code 
-        for i in range(4):
+        for i in range(4):                                                                          # draw secret code 
             self.draw_item(window, piecesecret, 175 + SQUARE_SIZE*i, 300)
 
-        # draw submit code section
-        for i in range(10):
-            self.draw_item(window, submitwaiting, 100, 350 + SQUARE_SIZE*i)
+        for i in range(MAX_ATTEMPTS):                                                               # draw submit code section
+            self.draw_item(window, submitwaiting, 100, 800 - SQUARE_SIZE*i)
 
-        # draw guess section
-        for row in range(10):
+        for row in range(MAX_ATTEMPTS):                                                             # draw guess section
             for col in range(4):
-                self.draw_item(window, piecewhite, 175  + SQUARE_SIZE*col, 350 + SQUARE_SIZE*row)
+                self.draw_item(window, piecewhite, 175  + SQUARE_SIZE*col, 800 - SQUARE_SIZE*row)
         
-        #draw clue section
-        for row in range(10):
+        for row in range(MAX_ATTEMPTS):                                                             # draw clue section
             for col in range(4):
-                self.draw_item(window, cluegray, 400  + SQUARE_SIZE/2*col, 350 + SQUARE_SIZE*row)
+                self.draw_item(window, cluegray, 400  + SQUARE_SIZE/2*col, 800 - SQUARE_SIZE*row)
 
     def draw_item(self, window, asset, x_pos, y_pos):
         window.blit(asset, (x_pos, y_pos))
 
-    def get_board(self):
-        return self.board
-
     def get_pieces(self):
         return self.pieces
+
+    def get_difficulty(self):
+        return self.difficulty
     
     def set_pieces(self):
+        difficulty = self.get_difficulty()
         if difficulty == 'Easy':
             self.pieces = easy_piece_collection
         elif difficulty == 'Normal':
@@ -65,14 +60,9 @@ class Board():
             self.pieces = hard_piece_collection
         
     def play_music(self):
-        # Starting the mixer
         pygame.mixer.init()
-        
-        # Loading the song
-        pygame.mixer.music.load("assets/music.mp3")
-        
-        # Start playing the song
-        pygame.mixer.music.play(-1)     # argument of -1 loops music
+        pygame.mixer.music.load("assets/music.mp3")    
+        pygame.mixer.music.play(-1)                     # play music; argument of -1 loops music
 
 
         
